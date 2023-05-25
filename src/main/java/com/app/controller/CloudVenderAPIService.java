c8fc15651415f650ceeeba54ec080874e9db508d
2ono <clt3555@gmail.com>
2023-05-25 13:08:38 +0900

    [add] put and delete method

--- src/main/java/com/app/controller/CloudVenderAPIService.java
+++ src/main/java/com/app/controller/CloudVenderAPIService.java
@@ -1,7 +1,9 @@
 package com.app.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +28,19 @@ public class CloudVenderAPIService {
 	public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
 		this.cloudVendor = cloudVendor;
 		return "Cloud Vendor Created Successfully";
+	}
+	
+	
+	@PutMapping
+	public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+		this.cloudVendor = cloudVendor;
+		return "Cloud Vendor Updated Successfully";
+	}
+	
+	@DeleteMapping("{vendorId}")
+	public String deleteCloudVendorDetails(String vendorId) {
+		this.cloudVendor = null;
+		return "Cloud Vendor Deleted Successfully";
 	}
 
 }
