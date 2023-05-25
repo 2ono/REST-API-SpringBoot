e14c8b34885107710546c2842bbb2a921a6a4573
2ono <clt3555@gmail.com>
2023-05-25 11:35:16 +0900

    [add] post method

--- src/main/java/com/app/controller/CloudVenderAPIService.java
+++ src/main/java/com/app/controller/CloudVenderAPIService.java
@@ -1,6 +1,8 @@
 package com.app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,11 +12,20 @@ import com.app.model.CloudVendor;
 @RequestMapping("/cloudvendor")
 public class CloudVenderAPIService {
 
+	CloudVendor cloudVendor;
+
 	@GetMapping("{vendorId}")
 	public CloudVendor getCloudVendorDetails(String vendorId) {
-		return new CloudVendor("C1", "Vendor 1",
-				"Address One", "xxxx");
+		return cloudVendor;
+
+//				new CloudVendor("C1", "Vendor 1",
+//				"Address One", "xxxx");
 	}
-	
-	
+
+	@PostMapping
+	public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor) {
+		this.cloudVendor = cloudVendor;
+		return "Cloud Vendor Created Successfully";
+	}
+
 }
